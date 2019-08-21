4c548ec03b8cf7410567f4d8886ec759c3d6699d
Gabor Hosszu <gabor@digitalasset.com>
2019-08-21 14:41:24 +0200

    Small adjustments

--- src/main/java/com/digitalasset/testing/utils/PackageUtils.java
+++ src/main/java/com/digitalasset/testing/utils/PackageUtils.java
@@ -148,7 +148,7 @@ public class PackageUtils {
       if (dt != null) {
         return dt;
       }
-      throw new IllegalArgumentException("No datatype found with the name " + moduleAndEntityName);
+      throw new IllegalArgumentException("No datatype found with the name '" + moduleAndEntityName + "'");
     }
   }
 
