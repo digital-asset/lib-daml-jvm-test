0bdd557ee99de152d059b91424b0a819bf41563e
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2019-09-04 15:15:03 +0200

    ERA-393 Fix constructor issue.

--- src/test/java/com/digitalasset/testing/steps/StepDefs.java
+++ src/test/java/com/digitalasset/testing/steps/StepDefs.java
@@ -1,5 +1,10 @@
 package com.digitalasset.testing.steps;
 
 import com.digitalasset.testing.cucumber.steps.LedgerInteractions;
+import com.digitalasset.testing.cucumber.utils.World;
 
-public class StepDefs extends LedgerInteractions {}
+public class StepDefs extends LedgerInteractions {
+  public StepDefs(World world) {
+    super(world);
+  }
+}
