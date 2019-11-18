da0026b3e21ff336e89f4d0b67cf3f40b922e3f5
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2019-11-18 16:43:59 +0100

    Added test for exposing sandbox port.

---
+++ src/test/java/com/digitalasset/testing/junit4/SandboxTest.java
@@ -0,0 +1,30 @@
+package com.digitalasset.testing.junit4;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+
+import java.nio.file.Paths;
+
+public class SandboxTest {
+
+  private static Sandbox sandbox =
+      Sandbox.builder().dar(Paths.get("./src/test/resources/ping-pong.dar")).build();
+
+  @ClassRule public static ExternalResource classRule = sandbox.getClassRule();
+
+  @Rule public ExternalResource rule = sandbox.getRule();
+
+  @Test
+  public void getSandboxPort() {
+    int sandboxPort = sandbox.getSandboxPort();
+    assertAreBetween(sandboxPort, 6860, 6890);
+  }
+
+  private void assertAreBetween(int x, int low, int high) {
+    String message = String.format("Expected '%d' to be between '%d' and '%d'", x, low, high);
+    Assert.assertTrue(message, low <= x && x <= high);
+  }
+}
