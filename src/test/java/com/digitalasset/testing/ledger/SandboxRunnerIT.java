1bf38d489de5eca7b342125a93ac68cb77aa2d5e
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-11 08:30:56 +0100

    Fixed imports.

--- src/test/java/com/digitalasset/testing/ledger/SandboxRunnerIT.java
+++ src/test/java/com/digitalasset/testing/ledger/SandboxRunnerIT.java
@@ -18,12 +18,11 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.digitalasset.testing.TestCommons.DAR_PATH;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class SandboxRunnerIT {
 
