4ebded094f573f0a05c10526c7fd51a916ad9996
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-17 15:40:45 +0100

    Applied scalafmt.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,14 +14,14 @@ object Dependencies {
   lazy val grizzledLogger = "org.clapper" %% "grizzled-slf4j" % "1.3.4"
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
   lazy val commonsIO = "commons-io" % "commons-io" % "2.6"
-  lazy val snakeYaml =  "org.yaml" % "snakeyaml" % yamlVersion
+  lazy val snakeYaml = "org.yaml" % "snakeyaml" % yamlVersion
   lazy val hamcrestOptional = "com.spotify" % "hamcrest-optional" % "1.1.4"
 
   lazy val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.24"
   lazy val guava = "com.google.guava" % "guava" % "28.0-jre"
 
   lazy val junit4 = "junit" % "junit" % "4.12"
-  lazy val junitInterface = "com.novocode" % "junit-interface" % "0.11" % Test exclude("junit", "junit-dep")
+  lazy val junitInterface = "com.novocode" % "junit-interface" % "0.11" % Test exclude ("junit", "junit-dep")
 
   lazy val cucumberJ8 = "io.cucumber" % "cucumber-java8" % cucumberVersion
   lazy val cucumberJunit = "io.cucumber" % "cucumber-junit" % cucumberVersion
