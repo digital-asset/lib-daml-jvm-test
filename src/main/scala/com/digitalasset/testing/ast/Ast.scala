7e039d2f5891fb0b7ecb2da6c4f37a561f2e4927
Akos Vandra <akos.vandra@digitalasset.com>
2020-11-30 20:19:22 +0100

    adjust as per discussion on func-test-framework

--- src/main/scala/com/digitalasset/testing/ast/Ast.scala
+++ src/main/scala/com/digitalasset/testing/ast/Ast.scala
@@ -6,6 +6,8 @@
 
 package com.digitalasset.testing.ast
 
+import PartialFunction._
+
 /**
   * ADTs representing typical hierarchical structure. This is the most generic common representation for YAML, JSON, and
   * other type-less hierarchical structures. DAML contracts can be represented using just these constructs.
@@ -16,4 +18,12 @@ object Ast {
   case class Map(value: collection.Map[String, Ast]) extends Ast
   case class Value(value: String) extends Ast
   case object Null extends Ast
+
+  object NullaryConstructor {
+    def unapply(ast: Ast): Option[String] = condOpt(ast) { case Constructor((key, Ast.Null)) => key }
+  }
+
+  object Constructor {
+    def unapply(ast: Ast): Option[(String, Ast)] = condOpt(ast) { case Map(map) if map.size == 1 => map.head }
+  }
 }
