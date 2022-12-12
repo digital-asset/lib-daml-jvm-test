d934a4ceb2742948e661693f650242205d906618
Andrew Fielder <90247335+fieldera-da@users.noreply.github.com>
2022-12-12 09:42:05 +0000

    uplift from daml 2.4.0 to 2.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,27 +1,27 @@
 import sbt._
 
 object Dependencies {
-  val DAML_SDK_VERSION = "2.4.0"
+  val DAML_SDK_VERSION = "2.4.2"
   val scalapbVersion = "0.9.2"
   val yamlVersion = "1.23"
   val cucumberVersion = "4.3.1"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
   lazy val damlJavaBinding = "com.daml" % "bindings-java" % DAML_SDK_VERSION
   lazy val damlLedgerClient = "com.daml" % "bindings-rxjava" % DAML_SDK_VERSION
   lazy val damlLFArchive = "com.daml" % "daml-lf-dev-archive-java-proto" % DAML_SDK_VERSION
 
   lazy val grizzledLogger = "org.clapper" %% "grizzled-slf4j" % "1.3.4"
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.5"
   lazy val commonsIO = "commons-io" % "commons-io" % "2.6"
   lazy val snakeYaml = "org.yaml" % "snakeyaml" % yamlVersion
-  lazy val hamcrestOptional = "com.spotify" % "hamcrest-optional" % "1.1.4"
+  lazy val hamcrestOptional = "com.spotify" % "hamcrest-optional" % "1.2.0"
 
-  lazy val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.24"
+  lazy val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.7"
   lazy val guava = "com.google.guava" % "guava" % "28.0-jre"
 
-  lazy val junit5 = "org.junit.jupiter" % "junit-jupiter-api" % "5.8.2"
-  lazy val junitInterface = "net.aichler" % "jupiter-interface" % "0.10.0" % Test
+  lazy val junit5 = "org.junit.jupiter" % "junit-jupiter-api" % "5.9.0"
+  lazy val junitInterface = "net.aichler" % "jupiter-interface" % "0.11.1" % Test
 
   lazy val cucumberJ8 = "io.cucumber" % "cucumber-java8" % cucumberVersion
   lazy val cucumberJunit = "io.cucumber" % "cucumber-junit" % cucumberVersion
