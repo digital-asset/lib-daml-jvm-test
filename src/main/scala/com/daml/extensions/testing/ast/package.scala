5d6d135187592ce340bf685fbfafe6a9afa22bef
Andrew Fielder <90247335+fieldera-da@users.noreply.github.com>
2022-12-20 12:29:05 +0000

    fix formatting

--- src/main/scala/com/daml/extensions/testing/ast/package.scala
+++ src/main/scala/com/daml/extensions/testing/ast/package.scala
@@ -16,18 +16,20 @@ import java.util.function.Function
 package object ast {
   def toAst(v: Value): Ast = v match {
     case null => Ast.Null
-    case r: DamlRecord => Ast.Seq(r.getFields.asScala.map(f => toAst(f.getValue)))
+    case r: DamlRecord =>
+      Ast.Seq(r.getFields.asScala.map(f => toAst(f.getValue)))
     case v: Variant    => Ast.Map(Map(v.getConstructor -> toAst(v.getValue)))
     case b: Bool       => Ast.Value(b.getValue.toString)
     case c: ContractId => Ast.Value(c.getValue)
-    case l: DamlList   => Ast.Seq(l.toList(Function.identity()).asScala.map(toAst))
-    case i: Int64      => Ast.Value(i.getValue.toString)
-    case d: Decimal    => Ast.Value(d.getValue.toString)
-    case n: Numeric    => Ast.Value(n.getValue.toString)
-    case t: Text       => Ast.Value(t.getValue)
-    case t: Timestamp  => Ast.Value(t.getValue.toString)
-    case p: Party      => Ast.Value(p.getValue)
-    case u: Unit       => Ast.Null
+    case l: DamlList =>
+      Ast.Seq(l.toList(Function.identity()).asScala.map(toAst))
+    case i: Int64     => Ast.Value(i.getValue.toString)
+    case d: Decimal   => Ast.Value(d.getValue.toString)
+    case n: Numeric   => Ast.Value(n.getValue.toString)
+    case t: Text      => Ast.Value(t.getValue)
+    case t: Timestamp => Ast.Value(t.getValue.toString)
+    case p: Party     => Ast.Value(p.getValue)
+    case u: Unit      => Ast.Null
     case o: DamlOptional if o.getValue.isPresent =>
       Ast.Map(Map(SomeCtor -> toAst(o.getValue.get))) //map(v => toAstJ(v)).orElseGet(() => Ast.Null)))
     case o: DamlOptional => Ast.Map(Map(SomeCtor -> Ast.Null))
